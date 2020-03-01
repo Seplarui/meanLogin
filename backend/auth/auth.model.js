4aae5db75875fceb166c238783cff61ca8257a66
Sebas <seplarui@gmail.com>
2020-03-01 14:04:11 +0100

    auth.model.js/server.js

--- backend/auth/auth.model.js
+++ backend/auth/auth.model.js
@@ -1,26 +1,26 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+mongoose.set('useCreateIndex', true)
 
-const userSchema= new Schema({
-    name:{
-        type:String,
-        require:true,
+const userSchema = new Schema({
+    name: {
+        type: String,
+        require: true,
         trim: true
     },
-    email:{
-        type:String,
-        require:true,
+    email: {
+        type: String,
+        require: true,
         trim: true,
         unique: true
     },
-    password:{
-        type:String,
-        require:true,
+    password: {
+        type: String,
+        require: true,
         trim: true
     }
-},{
-    timestamps:true
-}
+}, {
+    timestamps: true
+})
 
-
-)
+module.exports = userSchema
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,24 @@
 
 const authRoutes = require('./auth/auth.routes')
 const express = require('express')
+const properties = require('./config/properties')
+
+const DB = require('./config/db')
+//init DB
+DB()
 
 const app = express()
 const router = express.Router();
 
+app.use('/api', router)
+authRoutes(router)
 
-app.listen(3000, () => console.log(`Server runing on port 3000`))
+router.get('/', (req, res) => {
+    res.send('Hello from home')
+})
+
+app.use(router)
+
+
+app.listen(properties.PORT, () => console.log(`Server runing on port ${properties.PORT}`))
 
