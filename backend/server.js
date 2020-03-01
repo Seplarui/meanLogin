7547f040a7fc29658c4ee9f54c513c7a5f5cd13d
Sebas <seplarui@gmail.com>
2020-03-01 13:48:17 +0100

    index.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 'use strict'
 
+const authRoutes = require('./auth/auth.routes')
 const express = require('express')
+
 const app = express()
+const router = express.Router();
 
 
 app.listen(3000, () => console.log(`Server runing on port 3000`))
